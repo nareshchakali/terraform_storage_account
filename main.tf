e3c5bf0d090422bc1de73bd851983eb1bea600b8
naresh <naresh.ch@snp.com>
2023-07-14 15:38:25 +0530

    second commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=3.0.1"
+      version = "=3.65.0"
     }
   }
 }
@@ -37,4 +37,87 @@ resource "azurerm_storage_account" "sa" {
 }
 }
 
+resource "azurerm_mssql_server" "serverdemo" {
+  name = "${var.prefix}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location = "${azurerm_resource_group.rg.location}"
+  version = "12.0"
+  administrator_login = "snpadmin"
+  administrator_login_password = "snp@1234"
+}
 
+resource "azurerm_app_service_plan" "demo" {
+  name = "${var.prefix}-plan"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  kind = "Linux"
+  reserved = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name = "${var.prefix}-front"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  service_plan_id = azurerm_app_service_plan.demo.id
+
+
+  
+  connection_string {
+    name = "myconnection"
+    type = "SQLServer"
+    value = "Server=myserver;Database=mydatabase;User Id=myuser;Password=mypassword;"
+  }
+  site_config {
+    always_on = true
+  }
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.appi.instrumentation_key
+
+  }
+
+}
+
+resource "azurerm_log_analytics_workspace" "work" {
+  name = "${var.prefix}-workspace"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  sku = "PerGB2018"
+  retention_in_days = 30
+
+}
+
+resource "azurerm_application_insights" "appi" {
+  name = "${var.prefix}-appi"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  workspace_id = azurerm_log_analytics_workspace.work.id
+  application_type = "web"
+
+}
+
+
+
+
+resource "azurerm_linux_web_app" "back" {
+  name = "${var.prefix}-api-back"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  service_plan_id = azurerm_app_service_plan.demo.id
+
+
+site_config {
+  always_on = true
+
+}
+
+}
+
+
+
+
+  
